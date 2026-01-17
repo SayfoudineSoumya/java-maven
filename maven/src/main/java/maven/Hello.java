bacc4a79875799481b4aa089d96c1e5a66f04f75
soumaya <soumaya.sayfoudine@acelys.fr>
2026-01-17 10:16:34 +0100

    first setup in maven project

---
+++ maven/src/main/java/maven/Hello.java
@@ -0,0 +1,10 @@
+package maven;
+
+public class Hello {
+	
+	public static void main(String[] args)
+    {
+        System.out.println("Hello DevOps, Today is Sunday, 11 January 2026, V2");
+    }
+
+}
